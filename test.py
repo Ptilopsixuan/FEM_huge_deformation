fbcf1ef085ffccaeba19ad357a756d5482796329
Ptilopsixuan <a1928493774@163.com>
2025-06-01 15:01:08 +0800

    add create.py to create input file

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ model_name = "beam"
 input_path = os.path.join(os.getcwd(), "input", f"{model_name}.txt")
 model = reader.readFile(input_path)
 output = model.iterate()
-output_path = os.path.join(os.getcwd(), "output", f"{model_name}.txt")
+output_path = os.path.join(os.getcwd(), "output", f"{model_name}", f"{model_name}.txt")
 if not os.path.exists(os.path.dirname(output_path)):
     os.makedirs(os.path.dirname(output_path))
 for i, o in enumerate(output):
